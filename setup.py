2f8c8adfc816e6ac5bfdcebcad061458dfe85aa9
Mohamed Khalfella <mkhalfella@purestorage.com>
2020-07-20 19:06:51 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pmini',
+      version='0.1',
+      description='The is minimal python library',
+      url='http://github.com/khalfella/pmini',
+      author='Mohamed Khalfella',
+      author_email='khalfella@gmail.com',
+      license='MIT',
+      packages=['pmini'],
+      zip_safe=False)
